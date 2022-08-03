6b19a6158ba85f4140f7bf83ba82faf97996020c
tysuky <80823534+tysuky@users.noreply.github.com>
2022-08-03 11:55:45 -0300

    Update and rename loguin.js to coolibre3t8licence.js

---
+++ coolibre3t8licence.js
@@ -0,0 +1,6 @@
+
+// Script para fins educacionais - Não utilize profissionalmente
+function Login(){var done=0;var usuario=document.login.usuario.value;var senha=document.login.senha.value;
+if (usuario=="Stark" && senha=="kawe") { window.location="https://animesonline.cc/"; done=1; }
+if (usuario=="joão" && senha=="123") { window.location="https://blogger-profissional.blogspot.com/"; done=1; }
+if (done==0) { alert("Senha ou Usuário inválido."); }}
